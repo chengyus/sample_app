5aaf98c41c1c95c4208d76c57a68ec03ab8733cd
Joseph Sung <chengyu.sung@gmail.com>
2012-05-17 18:23:49 -0700

    removed Access-Control-Allow-Origin headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  after_filter :set_access_control_headers
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://js-consulting.from-la.net/'
-    headers['Access-Control-Request-Method'] = '*'
-  end
 end
