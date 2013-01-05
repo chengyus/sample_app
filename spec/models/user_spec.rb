fab644c9ff21c7c7a3452a8ff7db56fe7160b094
Joseph Sung <chengyu.sung@gmail.com>
2013-01-05 10:19:54 -0800

    updated user_spec, 6.3.2 done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,4 +79,14 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
 end
