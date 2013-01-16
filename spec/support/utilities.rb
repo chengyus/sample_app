15b7c10fa14a0807cc252ac8cb907c089959aa79
Joseph Sung <chengyu.sung@gmail.com>
2013-01-15 18:47:04 -0800

    before listing 9.10

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,3 +2,11 @@ include ApplicationHelper
 
 include UserUtil
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
